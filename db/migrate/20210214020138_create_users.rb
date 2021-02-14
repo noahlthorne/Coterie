b856d271b628fbb493053fb3610fa23464d0de71
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-02-14 11:09:10 +0900

    add table for user

---
+++ db/migrate/20210214020138_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.references :organization, null: false, foreign_key: true
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
